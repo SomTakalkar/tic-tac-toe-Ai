e47fcf7a15267abe76919df5543c451d10ccdf72
SomTakalkar <somtakalkar200211@gmail.com>
2024-12-12 13:50:02 +0530

    url for frontend

--- tictactoe_ai/server.js
+++ tictactoe_ai/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Enable CORS for your frontend
 app.use(cors({
-    origin: 'https://tic-tac-toe-ai-d9mp.vercel.app', // Updated frontend URL
+    origin: 'https://tic-tac-toe-ai-git-main-som-takalkars-projects.vercel.app/', // Updated frontend URL
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
